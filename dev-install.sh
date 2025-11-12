3d12718bdab9aa7ca2370e3f67ad47b1dfde7f2b
Yusuf Emre Bilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2025-11-12 22:23:58 +0300

    Initialize repository with modular installation script

---
+++ dev-install.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+readonly DEVENV_SCRIPT_PATH=$(dirname "${BASH_SOURCE[0]}")
+readonly DEVENV_SCRIPT_MODULES_DIR="${DEVENV_SCRIPT_PATH}/modules"
+readonly DEVENV_SCRIPT_MODULE_FUNC_NAME="run_module_installation"
+
+DRY_RUN=false
+
+log() {
+  if [[ "${DRY_RUN}" == true ]]; then
+    echo "[dry_run] ${@}"
+  else
+    echo "[default] ${@}"
+  fi
+}
+
+install_module() {
+  local module="${1}"
+  local module_script=$(basename "${module}")
+  source "${module}"
+  if ! declare -f ${DEVENV_SCRIPT_MODULE_FUNC_NAME} &>/dev/null; then
+    echo "${DEVENV_SCRIPT_MODULE_FUNC_NAME} is not defined for ${module_script}" >&2
+    return
+  fi
+
+  if [[ "${DRY_RUN}" == true ]]; then
+    log "Would run '${module_script}' installation script"
+    return
+  fi
+
+  "${DEVENV_SCRIPT_MODULE_FUNC_NAME}" | while IFS= read -r line; do
+    log "[${module_script}] ${line}" | sed 's/\] \[/|/g'
+  done
+
+  unset "${DEVENV_SCRIPT_MODULE_FUNC_NAME}"
+}
+
+install_all_modules() {
+  for module in "${DEVENV_SCRIPT_MODULES_DIR}"/*.sh; do
+    install_module "${module}"
+  done
+}
+
+if [[ "${1:-}" == "--dry-run" ]]; then
+  DRY_RUN=true
+fi
+
+cd "${DEVENV_SCRIPT_PATH}" && install_all_modules
