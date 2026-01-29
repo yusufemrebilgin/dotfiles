5740e731545840de3dc46c8386b11acf88d0d0cb
Yusuf Emre Bilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2026-01-29 20:20:08 +0300

    Add lsp configuration with plugins and settings

---
+++ configs/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,154 @@
+return {
+  'neovim/nvim-lspconfig',
+  dependencies = {
+    -- Automatically install LSPs and related tools to stdpath for Neovim
+    -- Mason must be loaded before its dependents so we need to set it up here.
+    -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
+    { 'mason-org/mason.nvim', opts = {} },
+    -- Useful status updates for LSP
+    { 'j-hui/fidget.nvim', opts = {} },
+    -- Allows extra capabilities provided by blink.cmp
+    'saghen/blink.cmp',
+
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+  },
+
+  config = function()
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup('user-lsp-attach', { clear = true }),
+      callback = function(event)
+        --
+        --  This function gets run when an LSP attaches to a particular buffer.
+        --
+        --    That is to say, every time a new file is opened that is associated with
+        --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
+        --    function will be executed to configure the current buffer
+        --
+        local map = function(keys, func, desc, mode)
+          mode = mode or 'n'
+          vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+        end
+
+        -- Rename the variable under your cursor.
+        map('grn', vim.lsp.buf.rename, 'Rename')
+
+        -- Execute a code action, usually your cursor needs to be on top of an error
+        -- or a suggestion from your LSP for this to activate.
+        map('gra', vim.lsp.buf.code_action, 'Goto Code Action', { 'n', 'x' })
+
+        -- WARN: This is not Goto Definition, this is Goto Declaration.
+        -- For example, in C this would take you to the header.
+        map('grD', vim.lsp.buf.declaration, 'Goto Declaration')
+
+
+        -- The following two autocommands are used to highlight references of the
+        -- word under your cursor when your cursor rests there for a little while.
+        --
+        -- See `:help CursorHold` for information about when this is executed
+        --
+        -- When you move your cursor, the highlights will be cleared (the second autocommand).
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
+        if client and client:supports_method('textDocument/documentHighlight', event.buf) then
+          local highlight_augroup = vim.api.nvim_create_augroup('user-lsp-highlight', { clear = false })
+          vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+            buffer = event.buf,
+            group = highlight_augroup,
+            callback = vim.lsp.buf.document_highlight,
+          })
+          vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+            buffer = event.buf,
+            group = highlight_augroup,
+            callback = vim.lsp.buf.clear_references,
+          })
+
+          vim.api.nvim_create_autocmd('LspDetach', {
+            group = vim.api.nvim_create_augroup('user-lsp-detach', { clear = true }),
+            callback = function(event2)
+              vim.lsp.buf.clear_references()
+              vim.api.nvim_clear_autocmds { group = highlight_augroup, buffer = event2.buf }
+            end,
+          })
+
+          vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
+          vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = true })
+          vim.keymap.set('i', '<C-Space>', function()
+            vim.lsp.completion.get()
+          end)
+        end
+
+        -- The following code creates a keymap to toggle inlay hints in your
+        -- code, if the language server you are using supports them
+        --
+        -- This may be unwanted, since they displace some of your code
+        if client and client:supports_method('textDocument/inlayHint', event.buf) then
+          map('<leader>th', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf }) end, 'Toggle Inlay Hints')
+        end
+      end
+    })
+
+    local capabilities = require('blink.cmp').get_lsp_capabilities()
+
+    local servers = {
+      gopls = {},
+      lua_ls = {},
+      ts_ls = {}
+    }
+
+    -- Ensure binaries are installed
+    --
+    -- To check the current status of installed tools and/or manually install
+    -- other tools, you can run
+    --    :Mason
+    --
+    -- You can press `g?` for help in this menu.
+    require('mason-tool-installer').setup({
+      ensure_installed = {
+        'gopls',
+        'lua-language-server',
+        'stylua',
+        'typescript-language-server'
+      }
+    })
+
+    for name, server in pairs(servers) do
+      server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+      vim.lsp.config(name, server)
+      vim.lsp.enable(name)
+    end
+
+
+    -- Special Lua Config, as recommended by neovim help docs
+    vim.lsp.config('lua_ls', {
+      on_init = function(client)
+        if client.workspace_folders then
+          local path = client.workspace_folders[1].name
+          if path ~= vim.fn.stdpath 'config' and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then
+            return
+          end
+        end
+
+        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+          runtime = {
+            version = 'LuaJIT',
+            path = { 'lua/?.lua', 'lua/?/init.lua' },
+          },
+          workspace = {
+            checkThirdParty = false,
+            -- NOTE: this is a lot slower and will cause issues when working on your own configuration.
+            --  See https://github.com/neovim/nvim-lspconfig/issues/3189
+            library = vim.api.nvim_get_runtime_file('', true),
+          },
+        })
+      end,
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { 'vim' },
+            disable = { 'undefined-field' }
+          }
+        },
+      },
+    })
+
+  end
+}
