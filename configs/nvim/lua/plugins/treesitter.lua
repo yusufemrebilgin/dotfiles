6efefbe05b53b39c610dec3693eb713e30573795
Yusuf Emre Bilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2026-07-08 10:29:03 +0300

    Update treesitter installation and language list

--- configs/nvim/lua/plugins/treesitter.lua
+++ configs/nvim/lua/plugins/treesitter.lua
@@ -1,32 +1,35 @@
 -- Nvim Treesitter (https://github.com/nvim-treesitter/nvim-treesitter)
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
   lazy = false,
   config = function()
-    require('nvim-treesitter.configs').setup({
-      modules = {},
-      indent = { enable = true },
-      highlight = { enable = true },
-      auto_install = false,
-      sync_install = false,
-      ignore_install = {},
-      ensure_installed = {
-        'bash',
-        'go',
-        'diff',
-        'lua',
-        'vim',
-        'vimdoc',
-        'markdown',
-        'markdown_inline',
-        'query',
-        'yaml',
-        'dockerfile',
-        'javascript',
-        'typescript'
-      }
+    require("nvim-treesitter").install({
+      "bash",
+      "c",
+      "diff",
+      "html",
+      "javascript",
+      "jsdoc",
+      "json",
+      "lua",
+      "luadoc",
+      "luap",
+      "markdown",
+      "markdown_inline",
+      "printf",
+      "go",
+      "java",
+      "python",
+      "query",
+      "regex",
+      "toml",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "xml",
+      "yaml",
     })
-  end
+  end,
 }
-
